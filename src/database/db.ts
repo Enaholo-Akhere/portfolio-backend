6b159fe973ab03db801b57aaeef346f8a3c2c1a5
Enaholo Akhere <enaholoa@gmail.com>
2023-11-20 18:58:29 +0100

    :lipstick: fix(package-json): added prod pass

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,12 +2,18 @@ import { Pool } from 'pg';
 import config from 'config'
 
 
+const db_name = config.get<string>('db_name');
+const pg_password = config.get<string>('pg_password');
+const pg_port = config.get<number>('pg_port');
+
+console.log('db name', db_name, pg_password, pg_port);
+
 const pool_dev = new Pool({
     user: 'postgres',
-    password: 'asdf',
+    password: pg_password,
     host: 'localhost',
-    database: config.get<string>('db_name'),
-    port: config.get<number>('pg_port'),
+    database: db_name,
+    port: pg_port,
 });
 
 const pool_prod = new Pool({
