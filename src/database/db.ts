e81bba5e383572139a2d144dd18b2a60fb840de0
Enaholo Akhere <enaholoa@gmail.com>
2023-11-20 19:26:06 +0100

    :lipstick: fix(package-json): added raw  details

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,18 +2,17 @@ import { Pool } from 'pg';
 import config from 'config'
 
 
-// const db_name = config.get<string>('db_name');
-const pg_password = config.get<string>('pg_password');
-const pg_port = config.get<number>('pg_port');
+// const pg_password = config.get<string>('pg_password');
+// const pg_port = config.get<number>('pg_port');
 
-console.log('db name', pg_password, pg_port);
+// console.log('db name', pg_password, pg_port);
 
 const pool_dev = new Pool({
     user: 'postgres',
-    password: pg_password,
+    password: 'asdf',
     host: 'localhost',
     database: 'my_portfolio',
-    port: pg_port,
+    port: 5600,
 });
 
 const pool_prod = new Pool({
